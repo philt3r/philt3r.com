9556125d605577054209de85216ebc67ae96499a
Philip Beadle <philip.beadle@live.com.au>
2016-11-25 17:43:02 +1100

    Added debug to Firebase deploy

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -17,7 +17,7 @@ then
 elif [[ ${TRAVIS_BRANCH} == *"master"* ]]
 then
 	echo "Master branch deploy to production"
-	firebase deploy --project "${FIREBASE_PROJECT_PROD}" --token "${FIREBASE_TOKEN_PROD}"
+	firebase deploy --debug --project "${FIREBASE_PROJECT_PROD}" --token "${FIREBASE_TOKEN_PROD}"
 else
 	echo "Unknown branch type...not deploying"
 fi
